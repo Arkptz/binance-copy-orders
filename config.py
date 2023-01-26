643cfb5159e8968455515654ce0be47b64b02772
Arkptz1 <58850875+Arkptz1@users.noreply.github.com>
2023-01-26 17:57:18 +0400

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ homeDir = (r'\\').join(os.path.abspath(__file__).split('\\')[:-1])
 admin_list = [1021524873]
 channel_id = -1001262628072
 BOT_TOKEN = '5825461628:AAFxLcq4B_z927H4JYvM11utplvRcML-_iQ' #@Bin_copy11_bot
-db_path = f'{homeDir}\\DB\\db.db'
-log_path = f'{homeDir}\\logs\\'
-SSL_CERT_FILE = f'{homeDir}\\ssl\\cacert.pem'
+db_path = f'{homeDir}\\DB\\db.db' if homeDir !='' else './DB/db.db'
+log_path = f'{homeDir}\\logs\\' if homeDir !='' else './logs/'
+SSL_CERT_FILE = f'{homeDir}\\ssl\\cacert.pem' if homeDir !='' else './ssl/cacert.pem'
