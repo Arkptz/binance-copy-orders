64bc0f1753ae82f59066cfb7ad270346e64788ea
Arkptz1 <58850875+Arkptz1@users.noreply.github.com>
2023-01-26 17:58:15 +0400

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ BOT_TOKEN = '5825461628:AAFxLcq4B_z927H4JYvM11utplvRcML-_iQ' #@Bin_copy11_bot
 db_path = f'{homeDir}\\DB\\db.db' if homeDir !='' else './DB/db.db'
 log_path = f'{homeDir}\\logs\\' if homeDir !='' else './logs/'
 SSL_CERT_FILE = f'{homeDir}\\ssl\\cacert.pem' if homeDir !='' else './ssl/cacert.pem'
+print(db_path, log_path, SSL_CERT_FILE)
