d884b618a80c965799c96cf7a33ea926a18add99
Arkptz1 <58850875+Arkptz1@users.noreply.github.com>
2023-01-30 23:36:12 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ admin_list = [1021524873, 828458879, 5675395916]
 channel_id = -1001262628072
 BOT_TOKEN = '5825461628:AAFxLcq4B_z927H4JYvM11utplvRcML-_iQ' #@Bin_copy11_bot
 db_path = f'{homeDir}\\DB\\db.db' if homeDir !='' else './DB/db.db'
-log_path = f'{homeDir}\\logs\\' if homeDir !='' else './logs/'
+log_path = f'{homeDir}\\logs\\' if homeDir !='' else '../binance-copy-orders/logs/'
 SSL_CERT_FILE = f'{homeDir}\\ssl\\cacert.pem' if homeDir !='' else './ssl/cacert.pem'
