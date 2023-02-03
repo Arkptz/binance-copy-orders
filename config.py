4fe2be7dceb022da0145988199765db10070eb5d
Arkptz1 <58850875+Arkptz1@users.noreply.github.com>
2023-02-03 09:06:36 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 homeDir = (r'\\').join(os.path.abspath(__file__).split('\\')[:-1])
-owners = [1021524873]
+owners = [1021524873, 828458879]
 admin_list = [1021524873, 828458879, 5675395916]
 channel_id = -1001262628072 #-1001793924567#
 BOT_TOKEN = '5825461628:AAFxLcq4B_z927H4JYvM11utplvRcML-_iQ' #@Bin_copy11_bot #'5593221166:AAEdAfjAiGERdXrFM2ySLsKBFVcSH9h8uFE'#@ukr_tt_bot #
